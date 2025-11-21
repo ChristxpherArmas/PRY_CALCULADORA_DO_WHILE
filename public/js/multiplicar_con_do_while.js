e8c31b6df90caa51c1a2dfe6849be47d791277e4
Christopher Armas <christopherarmas4@gmail.com>
2025-11-21 06:26:09 +0000

    inicializamos las  variables y listener del boton

---
+++ public/js/multiplicar_con_do_while.js
@@ -0,0 +1,7 @@
+const btn_calcular = document.getElementById('btn_calcular');
+const txt_resultado = document.getElementById('txt_resultado');
+const txt_alerta = document.getElementById('txt_alerta');
+
+btn_calcular.addEventListener('click',function(event){
+    
+});
